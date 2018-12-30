b8f1bc909b7ee801e0fd8a1b7e20a339a8a37220
Marco Klein <marco.klein94@gmail.com>
2018-12-30 17:54:00 +0100

    disabled physics debug graphics

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -304,7 +304,7 @@ export class GameScene extends Phaser.Scene {
         playerSprite1.setCollideWorldBounds(true);
         playerSprite1.setSize(64, 88);
 
-        //let player1InputController = new KeyboardController(this.input.keyboard.createCursorKeys());
+        //dlet player1InputController = new KeyboardController(this.input.keyboard.createCursorKeys());
         let player1InputController = new GamepadController(0);
 
         let player1 = new Player(player1InputController, playerSprite1, 'alienGreen');
